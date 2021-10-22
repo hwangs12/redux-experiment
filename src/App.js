6dbf4293f9143d214feb8c0fc0ecaf441f18da7b
hwangs12 <sungjun94@gmail.com>
2021-10-22 07:48:24 -0400

    connect components with connect

--- src/App.js
+++ src/App.js
@@ -5,16 +5,35 @@ import CartContainer from "./components/CartContainer";
 // items
 import cartItems from "./cart-items";
 // redux stuff
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+//reducer
+const reducer = (state, action) => {
+	return state;
+};
+
+//create initial store
+const initialStore = {
+	cartItems,
+	count: 21,
+};
+
+//create store, execute reducer
+const store = createStore(reducer, initialStore);
 
 function App() {
-  // cart setup
+	// cart setup
+	// create store
 
-  return (
-    <main>
-      <Navbar />
-      <CartContainer cart={cartItems} />
-    </main>
-  );
+	//setup initial store
+
+	return (
+		<Provider store={store}>
+			<Navbar />
+			<CartContainer />
+		</Provider>
+	);
 }
 
 export default App;
